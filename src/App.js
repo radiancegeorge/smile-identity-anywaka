9e6cb41ac55f15121b36add7b76491d032de9ff3
Radiance Obi <63566362+radiancegeorge@users.noreply.github.com>
2023-06-19 18:09:00 +0000

    "Initial commit"

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import "./styles.css";
 import axios from "axios";
 export default function App() {
   const handleVerify = async () => {
+    const product = "enhanced_kyc" // use the actual verification u would like to use in the platform and not this, also try to use something with some facial checks
     const { data } = await axios({
       method: "get",
-      url: "https://bf00-102-91-47-100.ngrok-free.app/user/verify/token",
+      url: "https://bf00-102-91-47-100.ngrok-free.app/user/verify/token", //use the actual server url please
       params: {
-        type: "enhanced_kyc"
+        type: product
       },
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJyYWRpYW5jZSIsImVtYWlsIjoicmFkaWFuY2VnZW9yZ2VAZ21haWwuY29tIiwicGhvbmVOdW1lYnIiOiIwODEzODU0NDAzNiIsImZ1bGxOYW1lIjoicmFkaWFuY2Ugb2JpIiwiaWF0IjoxNjg3MTg5OTIxfQ.pfTNfUNaos-_3nPHbMpsskcyUFYfNHZlY70zegF_9YE`
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJyYWRpYW5jZSIsImVtYWlsIjoicmFkaWFuY2VnZW9yZ2VAZ21haWwuY29tIiwicGhvbmVOdW1lYnIiOiIwODEzODU0NDAzNiIsImZ1bGxOYW1lIjoicmFkaWFuY2Ugb2JpIiwiaWF0IjoxNjg3MTk2ODk5fQ.zEu2EzWTHkBUjk6D_y9lQsykmcGT5r6Ir158ROM-foc`
       }
     });
-    const { token } = data;
+    const { token, callback_url } = data;
     console.log("calling...");
     window.SmileIdentity({
       token,
-      product: "enhanced_kyc",
-      callback_url: `https://bf00-102-91-47-100.ngrok-free.app/user/verify/`,
-      environment: "sandbox",
+      product, 
+      callback_url, // u will get this from the backend
+      environment: "sandbox", //remember to change this when u going production
       partner_details: {
-        partner_id: `2410`,
+        partner_id: "2410",
         name: `Anywaka`,
         // logo_url: `${your_app_logo_url}`,
-        policy_url: `https://bf00-102-91-47-100.ngrok-free.app/`,
-        theme_color: "blue"
+        policy_url: `https://bf00-102-91-47-100.ngrok-free.app/`, // note that this url goes nowhere, put an actual policy url
+        theme_color: "blue" //use anywaka's own color
       },
       onSuccess: console.log,
       onClose: console.log.bind(this, "closed"),
